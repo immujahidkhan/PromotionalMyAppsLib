2b13ff51c400caca792f907a00180527ec83ce4c
Mujahid.khan@hi5host.com,mohammed.nabeel@hi5host.com <immujahidkhan6@gmail.com>
2021-06-04 10:56:13 +0500

    :100:

--- PromoApps/src/main/java/com/justclack/promoapps/SessionControllers.kt
+++ PromoApps/src/main/java/com/justclack/promoapps/SessionControllers.kt
@@ -5,6 +5,7 @@ import android.content.SharedPreferences
 import androidx.core.content.edit
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import java.util.*
 
 class SessionControllers(private val context: Context) {
     companion object {
@@ -15,10 +16,9 @@ class SessionControllers(private val context: Context) {
 
     var poJoList: PoJo
         get() {
-            val jsonString = sharedPrefs.getString(
-                AdsList, PoJo().toString()
-            )
-            return gson.fromJson(jsonString, object : TypeToken<PoJoItem>() {}.type)
+            val json: String = sharedPrefs.getString(AdsList, PoJo().toString()).toString()
+            val type = object : TypeToken<PoJo>() {}.type
+            return gson.fromJson(json, type)
         }
         set(value) = sharedPrefs.edit {
             putString(AdsList, gson.toJson(value))
